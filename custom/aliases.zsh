8e93ab2d63a6f4d0ddd79e61046e6dc15b1da1a4
harryxu <harryzhxu@gmail.com>
2011-10-24 10:00:35 +0800

    Add custom scripts aliases and misc.
    misc.zsh: Add colorful man page.

---
+++ custom/aliases.zsh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+
+#commands
+alias ls="ls --color=auto"
+alias la="ls --color=always -a"
+alias ll="ls --color=always -lh"
+alias lla="ls --color=always -la"
+alias lal="ls --color=always -la"
+alias l='ls -CF'
+alias xopen='xdg-open'
+
